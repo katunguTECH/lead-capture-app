c249ff8b97c124ee4e78b82450efc1077b383d7f
Henry Munyoki <katungu1@gmail.com>
2025-08-14 12:02:43 +0300

    Add Landbot floating widget via useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
+import { useEffect } from "react";
+
 function App() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://cdn.landbot.io/landbot-3/landbot-3.0.0.js";
+    script.async = true;
+    script.onload = () => {
+      new window.Landbot.Livechat({
+        configUrl: "https://storage.googleapis.com/landbot.online/v3/H-3090419-7C0STPO35ZX7D4KW/index.json",
+      });
+    };
+    document.body.appendChild(script);
+  }, []);
+
   return (
     <div>
-      {/* Your app content */}
+      <h1>Welcome to Lead Capture App</h1>
     </div>
   );
 }
 
 export default App;
-
 
 
 
